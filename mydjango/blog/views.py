65a0f2ea668a07aeed0101012e8e8c0ad25c32b4
Yunsik Park <space214@gmail.com>
2017-05-31 17:00:46 +0900

    View 기능 구현

--- mydjango/blog/views.py
+++ mydjango/blog/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request):
+    # post_obj = Post.objects.all()
+    title = "블리자드 오버워치 영웅 프로필"
+    context = {
+        'post_list':Post.objects.all().order_by('-create_time'),
+        'title':  title
+    }
+    return render(request, 'blog/post_list.html', context)
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    context={
+        'title': post.title,
+        'text' : post.text
+    }
+
+    return render(request, 'blog/post_detail.html', context)
+
+def post_add(request):
+    if request.method == 'GET':
+        context = {
+
+        }
+        return render(request, 'blog/post_add.html', context)
+    elif request.method == 'POST':
+        user = get_user_model()
+        title = request.POST['title']
+        text = request.POST['text']
+        pt = Post.objects.create(
+            title=title,
+            text=text,
+            author = user.objects.first()
+        )
+        return redirect('post_detail', pk=pt.pk)
--- mydjango/mysite/urls.py
+++ mydjango/mysite/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from blog import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail,name = 'post_detail'),
+    url(r'^post/add/', views.post_add, name='post_add'),
 ]
